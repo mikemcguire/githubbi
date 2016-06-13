a5ff67aabf6a5e502e036934622db51bd4069f07
codewizard <mikemcguire.dev@gmail.com>
2016-06-13 13:32:50 -0600

    Fixing onclick binding to recycleview item & Making SearchActivity view look nicer

--- app/src/main/java/com/overexposeddesign/githubbi/adapters/ResultsViewAdapter.java
+++ app/src/main/java/com/overexposeddesign/githubbi/adapters/ResultsViewAdapter.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,9 +39,10 @@ public class ResultsViewAdapter extends RecyclerView.Adapter<RepositoryViewHolde
         RepositoryListItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
                 R.layout.repository_list_item, parent, true);
 
-        parent.findViewById(R.id.repository_item).setOnClickListener(new View.OnClickListener() {
+        binding.getRoot().findViewById(R.id.repository_item).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("click", "happened");
                 Intent intent = new Intent(view.getContext() ,ActivityWeb.class);
                 intent.putExtra("url", view.getTag().toString());
                 view.getContext().startActivity(intent);
